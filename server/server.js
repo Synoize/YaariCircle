82ce081f754799e203cff24d2c76f36664197c8d
synoize <suryavanshivam@gamil.com>
2025-11-14 05:36:37 +0530

    added reuired inputs

--- server/server.js
+++ server/server.js
@@ -19,6 +19,9 @@ const corsOptions = {
     origin: [
         "http://localhost:5174",
         "http://localhost:5173",
+        "https://yaaricircle.com/",
+        "https://yaaricircle.vercel.app/",
+        "https://yaaricircle-admin.vercel.app/",
     ],
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true,
